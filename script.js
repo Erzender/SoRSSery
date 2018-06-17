cf2b172f368929667c865c6214d2a44f3b186eab
Erz <>
2018-06-17 18:02:51 +0200

    Okay we can now display news and scroll through them

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ function xmlToJson(xml) {
 	var obj = {};
 	if (xml.nodeType == 1) {
 		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
 		for (var j = 0; j < xml.attributes.length; j++) {
                 var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+				obj[attribute.nodeName] = attribute.nodeValue;
 			}
 		}
 	} else if (xml.nodeType == 3) {
@@ -31,20 +30,14 @@ function xmlToJson(xml) {
 	return obj;
 };
 
-function fetchRss(url) {
-	
-	feednami.load(url,function(result){
-		if(result.error) {
-			console.log(result.error);
-		} else {
-			var entries = result.feed.entries;
-			for(var i = 0; i < entries.length; i++){
-				var entry = entries[i];
-				console.dir(entry.title);
-			}
-		}
-	});
-}
+/*
+[ "title", "description", "summary", 
+"date", "pubdate", "pubDate", "link", "guid", "author", "comments", 
+"origlink", "image", "source", "categories", "enclosures", 
+"atom:@", "atom:id", "yt:videoid", "yt:channelid", "atom:title", 
+"atom:link", "atom:author", "atom:published", "atom:updated", 
+"media:group", "pubdate_ms", "date_ms" ]
+*/
 
 var app = new Vue({
     el: "#app",
@@ -52,9 +45,55 @@ var app = new Vue({
         page: "feed",
         topics: [],
 		selectedTopic: null,
-		feed: []
-    },
+		feed: [],
+		slice: 15
+	},
+	computed: {
+		news: function() {
+			if (this.topics[this.selectedTopic] === undefined) {
+				return []
+			}
+			return this.feed.filter(function(elem) {
+				return elem.topic === this.title
+			}, this.topics[this.selectedTopic]).map(article => { return {
+				...article.data,
+				dateL: moment( article.data.pubdate).fromNow()
+			}}).sort((a, b) => new Date(b.pubdate) - new Date(a.pubdate)).slice(0, this.slice)
+		}
+	},
     methods: {
+		scroll: function(key) {
+			var elmnt = document.getElementById("feed");
+			switch (key) {
+				case 'up':
+					elmnt.scrollTop = elmnt.scrollTop - 200
+					return
+				case 'down':
+					elmnt.scrollTop = elmnt.scrollTop + 200
+					return
+				case 'pageup':
+					elmnt.scrollTop = elmnt.scrollTop - 1000
+					return
+				case 'pagedown':
+					elmnt.scrollTop = elmnt.scrollTop + 1000
+					return
+				case 'bottom':
+					elmnt.scrollTop = elmnt.scrollHeight
+					return
+				case 'top':
+					elmnt.scrollTop = 0
+					return
+				case 'check':
+					if (elmnt.scrollTop >= elmnt.scrollHeight - 800 && this.slice < this.feed.filter(function(elem) {
+						return elem.topic === this.title
+					}, this.topics[this.selectedTopic]).length){
+						this.slice = this.slice + 15
+					}
+					return
+				default:
+					break;
+			}
+		},
         readSingleFile: function(e) {
             var file = e.target.files[0];
             if (!file) {
@@ -72,10 +111,13 @@ var app = new Vue({
             reader.readAsText(file);
         },
         changePage: function(page) {
-            this.page = this.page==="feed"?"settings":"feed"
+            this.page = this.page==="feed"?"settings":"feed";
         },
         changeTopic: function(topic) {
-            this.selectedTopic = topic
+			var elmnt = document.getElementById("feed");
+			elmnt.scrollTop = 0
+			this.selectedTopic = topic;
+			this.slice = 15
 		},
 		prepareFeed: function() {
 			if (!this.topics || !this.topics.length>0) {
@@ -84,24 +126,45 @@ var app = new Vue({
 				return
 			}
 			this.selectedTopic = 0
-			for (var topic of this.topics) {
-				this.feed.push({key: topic["@attributes"].title, feed: []})
-			}
 		},
 		fetchFeed: function() {
 			for (var topic of this.topics) {
-				if (topic.outline["@attributes"]) {
-					console.log(JSON.stringify(topic.outline["@attributes"].xmlUrl));
-					fetchRss(topic.outline["@attributes"].xmlUrl, topic["@attributes"].title)
+				if (topic.outline.title) {
+					this.fetchRss(topic.outline.xmlUrl, topic.title)
 				} else {
 					for (elem of topic.outline) {
-						fetchRss(elem["@attributes"].xmlUrl, topic["@attributes"].title)
+						this.fetchRss(elem.xmlUrl, topic.title)
 					}
 				}
 			}
+		},
+		fetchRss: function(url, title) {
+			feednami.load(url,function(result){
+				if(result.error) {
+					console.log(result.error);
+					console.log(url);
+				} else {
+					var entries = result.feed.entries;
+					for(var i = 0; i < entries.length; i++){
+						var entry = entries[i];
+						this.test = entry.title;
+						if (!this.diff(entry.title, entry.url)) {
+							this.feed.push({topic: title, data: entry})
+						}
+					}
+				}
+			}.bind(this));
+		},
+		diff: function(title, url) {
+			for (elem of this.feed) {
+				if (elem.data.title===title && elem.data.url===url) {
+					return true
+				}
+			}
+			return false
 		}
     },
-    beforeMount: function(){
+    mounted: function(){
 		this.topics=localStorage.topics?JSON.parse(localStorage.topics):[]
 		this.prepareFeed()
 		this.fetchFeed()
